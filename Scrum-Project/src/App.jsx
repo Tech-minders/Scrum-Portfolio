90a101d770371f6cfc2999487f8efc937a385861
Tech-minders <lucynjoka2025@gmail.com>
2026-02-16 15:22:43 +0300

    edited projects.jsx

--- Scrum-Project/src/App.jsx
+++ Scrum-Project/src/App.jsx
@@ -10,7 +10,7 @@ return (
 <nav>
 <Link to="/">Home</Link> |
 <Link to="/about">About</Link> |
-<Link to="/projects">Projects</Link> |
+<Link to="/projects">My Projects</Link> |
 <Link to="/contact">Contact</Link> |
 <Link to="/profile">Profile</Link>
 </nav>
