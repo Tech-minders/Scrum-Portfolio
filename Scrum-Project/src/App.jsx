0b8b6716aa0737e5395682543dc217d0c444b12f
Tech-minders <lucynjoka2025@gmail.com>
2026-02-16 15:02:45 +0300

    PORT-2: setup routing

--- Scrum-Project/src/App.jsx
+++ Scrum-Project/src/App.jsx
@@ -1,12 +1,27 @@
-
-
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
+import Profile from './pages/Profile'
 function App() {
-
-  return (
-    <div>
-      hello
-    </div>
-  )
+return (
+<div>
+<nav>
+<Link to="/">Home</Link> |
+<Link to="/about">About</Link> |
+<Link to="/projects">Projects</Link> |
+<Link to="/contact">Contact</Link> |
+<Link to="/profile">Profile</Link>
+</nav>
+<Routes>
+<Route path="/" element={<Home />} />
+<Route path="/about" element={<About />} />
+<Route path="/projects" element={<Projects />} />
+<Route path="/contact" element={<Contact />} />
+<Route path="/profile" element={<Profile />} />
+</Routes>
+</div>
+)
 }
-
 export default App
